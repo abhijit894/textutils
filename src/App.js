782e028bffa0a5928b09ac2d3d790038eacb6da2
Abhijit Paul <abhijitpaulfatepur@gmail.com>
2025-06-25 14:31:51 +0530

    update final

--- src/App.js
+++ src/App.js
@@ -1,68 +1,50 @@
-import { useState } from "react";
-import "./App.css";
-import About from "./components/About";
-import Navbar from "./components/Navbar";
-import TextForm from "./components/TextForm";
-import Alert from "./components/Alert";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import About from './components/About';
+import React, { useState } from 'react';
+import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
 
 function App() {
-  const [mode, setMode] = useState("light");
+  const [mode, setMode] = useState('light');
   const [alert, setAlert] = useState(null);
 
-  const showAlert = (messege, type) => {
+  const showAlert = (message, type) => {
     setAlert({
-      msg: messege,
-      type: type,
+      msg: message,
+      type: type
     });
     setTimeout(() => {
       setAlert(null);
     }, 1500);
   };
 
-  const removeBodyClasses= ()=>{
-    document.body.classList.remove('bg-light')
-    document.body.classList.remove('bg-dark')
-    document.body.classList.remove('bg-warning')
-    document.body.classList.remove('bg-danger')
-    document.body.classList.remove('bg-success')
-  }
-
-  const toggleMode = (cls) => {
-    removeBodyClasses(cls)
-    document.body.classList.add('bg-' +cls)
-
-    if (mode === "light") {
-      setMode("dark");
-      document.body.style.backgroundColor = "#042743";
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
       showAlert("Dark mode has been enabled", "success");
-      document.title = "Textutils - Dark Mode";
     } else {
-      setMode("light");
-      document.body.style.backgroundColor = "white";
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
       showAlert("Light mode has been enabled", "success");
-      document.title = "Textutils - Light Mode";
     }
   };
 
   return (
     <>
       <Router>
-        <Navbar title="Textutils" mode={mode} toggleMode={toggleMode} />
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} key={new Date()} />
         <Alert alert={alert} />
         <div className="container my-3">
           <Routes>
-            <Route path="/about" element={<About mode={mode} />} />
-            <Route
-              path="/"
-              element={
-                <TextForm
-                  showAlert={showAlert}
-                  heading="Enter the text below"
-                  mode={mode}
-                />
-              } 
-            />
+            <Route exact path="/about" element={<About mode={mode} />} />
+            <Route exact path="/" element={<TextForm showAlert={showAlert} heading="TextUtils - word counter, character counter, remove extra spaces" mode={mode} />} />
           </Routes>
         </div>
       </Router>
--- src/components/About.js
+++ src/components/About.js
@@ -1,99 +1,60 @@
-import React, { useState } from "react";
+import React from 'react'
 
 export default function About(props) {
 
-    // const [mystyle, setMyStyle] = useState(
-    //   {
+    // const [myStyle, setMyStyle] = useState({
     //     color: 'black',
     //     backgroundColor: 'white'
-    // })
-
-    let mystyle = {
-      color: props.mode === 'dark'?'white' : '#042743',
-      backgroundColor: props.mode === 'dark'? 'rgb(36 74 104)' : 'white',
-      border : '2px solid',
-      borderColor: props.mode === 'dark'?'white' : '#042743'
+    // }) 
+    let myStyle = {
+        color: props.mode ==='dark'?'white':'#042743',
+        backgroundColor: props.mode ==='dark'?'rgb(36 74 104)':'white', 
     }
     
-  return (
-    <div ClassNameName="Container " style={{color: props.mode === 'dark'?'white' : '#042743'}}>
-      <h2 ClassNameName="my-10">About us</h2>
-      <div ClassName="accordion" id="accordionExample">
-        <div ClassName="accordion-item">
-          <h2 ClassName="accordion-header">
-            <button
-              ClassName="accordion-button"
-              type="button"style={mystyle}
-              data-bs-toggle="collapse"
-              data-bs-target="#collapseOne"
-              aria-expanded="true"
-              aria-controls="collapseOne"
-            >
-              Analyze Your Text
-            </button>
-          </h2>
-          <div
-            id="collapseOne"
-            ClassName="accordion-collapse collapse show"
-            data-bs-parent="#accordionExample"
-          >
-            <div ClassName="accordion-body" style={mystyle}>
-              <strong>This is the first item’s accordion body.</strong> It is
-               Quickly analyze your text for word count, character count, and reading time.
+    return (
+        <div className="container">
+            <h1 className="my-3" style={{color: props.mode ==='dark'?'white':'#042743'}}>About Us</h1>
+            <div className="accordion" id="accordionExample">
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="headingOne">
+                    <button className="accordion-button" type="button" style={myStyle} data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    <strong>Analyze Your text </strong>
+                    </button>
+                    </h2>
+                    <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                    <div className="accordion-body" style={myStyle}>
+                    Textutils gives you a way to analyze your text quickly and efficiently. Be it word count, character count or 
+
+                    </div>
+                    </div>
+                </div>
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="headingTwo">
+                    <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                    <strong>Free to use </strong>
+                    </button>
+                    </h2>
+                    <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                    <div className="accordion-body" style={myStyle}>
+                    TextUtils is a free character counter tool that provides instant character count & word count statistics for a given text. TextUtils reports the number of words and characters. Thus it is suitable for writing text with word/ character limit.
+                    </div>
+                    </div>
+                </div>
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="headingThree">
+                    <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                    <strong>Browser Compatible </strong>
+                    </button>
+                    </h2>
+                    <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                    <div className="accordion-body" style={myStyle}>
+                    This word counter software works in any web browsers such as Chrome, Firefox, Internet Explorer, Safari, Opera. It suits to count characters in facebook, blog, books, excel document, pdf document, essays, etc.
+
+                    </div>
+                    </div>
+                </div>
             </div>
-          </div>
+
         </div>
-        <div ClassName="accordion-item" >
-          <h2 ClassName="accordion-header">
-            <button
-              ClassName="accordion-button collapsed"style={mystyle}
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#collapseTwo"
-              aria-expanded="false"
-              aria-controls="collapseTwo"
-            >
-              Free to Use
-            </button>
-          </h2>
-          <div
-            id="collapseTwo"
-            ClassName="accordion-collapse collapse"
-            data-bs-parent="#accordionExample"
-          >
-            <div ClassName="accordion-body" style={mystyle}>
-              TextUtils is completely free and available for everyone.
-            </div>
-          </div>
-        </div>
-        <div ClassName="accordion-item">
-          <h2 ClassName="accordion-header">
-            <button
-              ClassName="accordion-button collapsed"style={mystyle}
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#collapseThree"
-              aria-expanded="false"
-              aria-controls="collapseThree"
-            >
-              Browser Compatible 
-            </button>
-          </h2>
-          <div
-            id="collapseThree"
-            ClassName="accordion-collapse collapse"
-            data-bs-parent="#accordionExample"
-          >
-            <div ClassName="accordion-body" style={mystyle}>
-               Works seamlessly on all modern web browsers without any installation.
-            </div>
-          </div>
-        </div>
-      </div>
-      {/* <div ClassName="my-3">
-        <button onClick={toggleStyle} type="button" ClassName="btn btn-primary">{btntext}</button>
-      </div> */}
-    
-    </div>
-  );
+    )
 }
