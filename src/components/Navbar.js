bf4dae934d5c9e27c47bc47faa923ddd48cb3463
Abhijit Paul <abhijitpaulfatepur@gmail.com>
2025-07-21 11:07:36 +0530

    last update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -28,7 +28,7 @@ export default function Navbar(props) {
                                checked={props.mode === 'dark'}
                         />
                         <label className="form-check-label" htmlFor="flexSwitchCheckDefault">
-                            EnableDarkMode
+                            Enable Dark Mode
                         </label>
                     </div>
                 </div>
